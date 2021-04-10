ea4ed5bd55fc99d0f4c440cad28c30c1f35e7a48
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-04-10 13:54:18 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function L (){
     localStorage.setItem("player_2", p2);
     window.location = "GP.html"
    }
+   function N(){
+       n1 = document.getElementById("n1").value;
+       n2 = document.getElementById("n2").value;
+       localStorage.setItem("No.1", n1);
+       localStorage.setItem("No.2", n2);
+   }
+
+   function B(){
+       window.location = "index.html";
+   }
