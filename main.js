9b256be270c85333572eaa1b05491e59719debe2
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-08-02 18:45:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+Qt="player 1"
+At="player 2"
+p1S = 0;
+p2S = 0;
 function L (){
     p1 = document.getElementById("t1").value;
     p2 = document.getElementById("t2").value;
@@ -5,6 +9,7 @@ function L (){
     localStorage.setItem("player_2", p2);
     window.location = "GP.html"
    }
+   
    function N(){
        n1 = document.getElementById("n1").value;
        n2 = document.getElementById("n2").value;
@@ -20,4 +25,25 @@ document.getElementById("n2").value = "";
 
    function B(){
        window.location = "index.html";
+   }
+   function check(){
+        gA = document.getElementById("icb").value;
+        if (gA == AccA) {
+             if (At == "player 1") {
+                 up1s = p1S + 1;
+                 document.getElementById("p1s").innerHTML = up1s;
+
+             } else {
+                up1s = p1S + 1;
+                document.getElementById("p1s").innerHTML = up1s;
+             }
+             if (Qt == "player 1") {
+                 Qt = "player 2"
+                 document.getElementById("qt").innerHTML = p2;
+             } else {
+                Qt = "player 2"
+                document.getElementById("qt").innerHTML = p1;
+             }
+            
+        }
    }
